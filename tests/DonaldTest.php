5d940fec7986cda002989163384ec21d71e7f01c
D'haemer Frederik <Frederik.Dhaemer@digipolis.gent>
2018-05-04 10:50:06 +0200

    Adding test logic

--- tests/DonaldTest.php
+++ tests/DonaldTest.php
@@ -3,9 +3,20 @@
 namespace Test\Quote;
 
 use PHPUnit\Framework\TestCase;
+use Quote\Donald;
 
 class DonaldTest extends TestCase {
+  private $donald;
+
+  public function setUp() {
+    $this->donald = new Donald();
+  }
+
   public function testSpeak() {
-    // implement test
+    $this->assertInternalType(
+      'string',
+      $this->donald->random()
+    );
+
   }
 }
